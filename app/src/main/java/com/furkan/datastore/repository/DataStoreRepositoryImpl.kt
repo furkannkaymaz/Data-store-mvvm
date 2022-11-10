9cd1bdf54993070d209e3f1c45f51116bdfaf3fe
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-11-10 22:42:02 +0300

    code review

--- app/src/main/java/com/furkan/datastore/repository/DataStoreRepositoryImpl.kt
+++ app/src/main/java/com/furkan/datastore/repository/DataStoreRepositoryImpl.kt
@@ -109,5 +109,4 @@ class DataStoreRepositoryImpl
         }
     }
 
-
 }
--- app/src/main/java/com/furkan/datastore/utils/PreferenceHelper.kt
+++ app/src/main/java/com/furkan/datastore/utils/PreferenceHelper.kt
@@ -1,11 +1,15 @@
 package com.furkan.datastore.utils
 
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 
 const val USER_PREFERENCES_NAME = "furkan_app"
 
 object PreferenceHelper {
 
     val APP_NAME = stringPreferencesKey("APP_NAME")
+    val BOOLEAN_VALUE = booleanPreferencesKey("BOOLEAN_VALUE")
+    val FLOAT_VALUE = floatPreferencesKey("FLOAT_VALUE")
+    val LONG_VALUE = longPreferencesKey("LONG_VALUE")
+    val INT_VALUE = intPreferencesKey("INT_VALUE")
 
 }
