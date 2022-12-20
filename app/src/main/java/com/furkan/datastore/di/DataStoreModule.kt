64314a88762af7dd090c2cc0e614d9b2404afce0
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-12-21 00:35:48 +0300

    data store module code review

--- app/src/main/java/com/furkan/datastore/di/DataStoreModule.kt
+++ app/src/main/java/com/furkan/datastore/di/DataStoreModule.kt
@@ -12,14 +12,13 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+val Context.dataStore by preferencesDataStore(
+    name = USER_PREFERENCES_NAME,
+)
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
-
-    val Context.dataStore by preferencesDataStore(
-        name = USER_PREFERENCES_NAME,
-    )
 
     @Singleton
     @Provides
